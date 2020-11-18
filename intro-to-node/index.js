6524006c63e35c44546af38734f4cc36ac612f2f
chin <mcaiox@icloud.com>
2020-11-18 11:21:34 +0000

    Updated folder structure

--- intro-to-node/index.js
+++
@@ -1,9 +0,0 @@
-const superHeroes = require('superheroes');
-const superVillain = require('superVillains');
-
-
-var mySH = superHeroes.random()
-var mySV = superVillain.random()
-
-
-console.log(mySH)
